649f5184e396e4c41badff3828c1bf8f0e04fc9a
johnnia001 <jarroyo.staff@gmail.com>
2022-01-25 23:59:11 -0700

    fix styling on rules page

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './rules.scss'
 import gif from './gif/gif.gif'
 const Rules = () => {
   return (
     <>
-      <h1>Please be nice</h1>
-      <img
+      <h1 className='rules'>please be nice</h1>
+      <img className= 'rulesSock'
         src={gif}
         alt='new'
       />
