b8d07cc320ab5b69ff8077a857d7482d42629744
TheLenaburger <austin.lenaburg@gmail.com>
2022-01-26 19:26:56 -0600

    centers Socket in our rules page

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,11 +4,10 @@ import gif from './gif/gif.gif'
 const Rules = () => {
   return (
     <>
-      <h1 className='rules'>please be nice</h1>
-      <img className= 'rulesSock'
-        src={gif}
-        alt='new'
-      />
+      <div className='rulesSock'>
+        <h1 className='rules'>please be nice</h1>
+        <img className='socket' src={gif} alt='new' />
+      </div>
     </>
   )
 }
